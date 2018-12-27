0693476c5606d4e0107d24179ade07ad279de1ab
Nabil Ridhwanshah <nabilrdhwn@gmail.com>
2018-12-27 10:11:08 +0800

    Finished with login system

--- main.js
+++ main.js
@@ -2,10 +2,37 @@ let username = $("#username");
 let password = $("#password");
 let login_button = $("#login_button");
 
+let users = [{
+        user: 'cooler',
+        pass: 'suckingonthisdick',
+        sub: 'none',
+    },
+
+    {
+        user: 'renabil',
+        pass: 'gayass',
+        sub: 'admin',
+    }
+]
+
+let currentState = {};
+
 login_button.on('click', () => {
-    console.log('LOGIN BUTTON PRESSED')
-    console.log({
-        'username': username.val(),
-        'password': password.val(),
-    })
+    // LOOP THROUGH THE ARRAY
+    for (let x = 0; x < users.length; x++) {
+
+        // CHECK IF THE USERNAME AND PASSWORD MATCHES ANY OF THE USERS
+        if (username.val() == users[x].user && password.val() == users[x].pass) {
+            console.log("LOGGED IN")
+
+            currentState = {
+                user: users[x].user,
+                sub: users[x].sub,
+                last_logged: new Date()
+            }
+            
+        } else {
+            console.log('WRONG USERNAME OR PASSWORD')
+        }
+    }
 })
